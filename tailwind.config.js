d31eca6f03e9e8ed4c2dcdd77264f1a6a4d245d9
Sara <sarajosephperez@gmail.com>
2024-06-30 14:05:30 +0200

    add standard lint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,20 @@
 /* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/*.tsx", "./src/**/*.tsx"],
+  content: ['./src/*.tsx', './src/**/*.tsx'],
   plugins: [
-    require("@tailwindcss/typography"),
-    require("daisyui")
+    require('@tailwindcss/typography'),
+    require('daisyui')
   ],
-   // daisyUI config (optional - here are the default values)
-   daisyui: {
-    themes: ["dark", "light"],
-    darkTheme: "dark", // name of one of the included themes for dark mode
+  // daisyUI config (optional - here are the default values)
+  daisyui: {
+    themes: ['dark', 'light'],
+    darkTheme: 'dark', // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
-    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    prefix: '', // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
     logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
-    themeRoot: ":root", // The element that receives theme color CSS variables
-  },
+    themeRoot: ':root' // The element that receives theme color CSS variables
+  }
 }
-
